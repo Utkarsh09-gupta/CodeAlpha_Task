420c283174d5d6e909c3bc87b155c9cdee143cfd
Utkarsh09-gupta <utkarshgupta0518@gmail.com>
2025-07-10 05:14:04 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let buttons = document.querySelectorAll
 let string = "";
 let arr = Array.from(buttons);
 arr.forEach(button => {
-	button.addEventlistener('click', (e) => {
+	button.addEventListener('click', (e) => {
 		if(e.target.innerHTML == '='){
 			string = eval(string);
 			input.value = string;
